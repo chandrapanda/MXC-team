146522b905bd037b702f236fcf75ac8547c50cb9
Emmanuel Lyimo <esal29t@gmail.com>
2021-12-11 13:34:06 -0800

    modified script.js

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,29 @@
 var catFactsButton = document.querySelector("#cat-facts");
 var dogPicsButton = document.querySelector("#dog-pics");
 var funnyJokesButton = document.querySelector("#funny-jokes");
+var catFact = document.createElement("p");
 
 function getCatFact() {
     var catFactAPI = "https://catfact.ninja/fact";
     fetch(catFactAPI)
+    .then(response => response.json())
+    .then(function(data) {
+        displayCatFact(data)
+    });
+
+    document.getElementById("button-container").style.display = "none";
+}
+
+function displayCatFact(kittyStuff) {
+    var firstCatFact = kittyStuff.fact;
+    document.body.appendChild(catFact);
+    catFact.innerHTML = firstCatFact;
+    console.log(kittyStuff.fact);
+}
+
+function getDogPicture() {
+    var getDogAPI = "https://dog.ceo/api/breeds/image/random";
+    fetch(getDogAPI)
     .then(response => response.json())
     .then(data => console.log(data));
 }
@@ -18,9 +37,12 @@ funnyJokesButton.addEventListener("click",funnyJokes);
        fetch(jokes)
        .then(response => response.json())
        .then(data => console.log(data));
-       
+       document.getElementById("button-container").style.display = "none"; 
    };
 
+dogPicsButton.addEventListener("click", getDogPicture );
+
+// funnyJokesButton.addEventListener("click", );
 
 catFactsButton.addEventListener("click", getCatFact);
 
