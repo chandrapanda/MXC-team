2c42db58b0e58eeab10e7eb10fb9a42958876945
Luke <lukeconrad.stew@gmail.com>
2021-12-13 21:10:30 -0800

    nav bar added

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,82 @@
 var catFactsButton = document.querySelector("#cat-facts");
 var dogPicsButton = document.querySelector("#dog-pics");
 var funnyJokesButton = document.querySelector("#funny-jokes");
+var catFact = document.createElement("p");
 
 function getCatFact() {
     var catFactAPI = "https://catfact.ninja/fact";
     fetch(catFactAPI)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(function(data) {
+        displayCatFact(data)
+    });
+
+    document.getElementById("button-container").style.display = "none";
+    document.getElementById("back-button").style.display = "block";
+}
+
+function displayCatFact(kittyStuff) {
+    var firstCatFact = kittyStuff.fact;
+    document.body.appendChild(catFact);
+    catFact.innerHTML = firstCatFact;
 }
 
 function getDogPicture() {
     var getDogAPI = "https://dog.ceo/api/breeds/image/random";
     fetch(getDogAPI)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(function(data) {
+        displayDogPicture(data)
+    });
+
+    document.getElementById("button-container").style.display = "none";
+    document.getElementById("back-button").style.display = "block";
+}
+
+function displayDogPicture(doggyStuff) {
+    var firstDogpicture = doggyStuff.message;
+    // we should discuss changing "catFact" to a more universal name
+    document.body.appendChild(catFact);
+    catFact.innerHTML ='<img src="' + firstDogpicture + '" alt="a random image of a dog">';
 }
 
 
+var backButton = document.getElementById("back-button");
+backButton.addEventListener("click", goBackToMainPage);
+
+funnyJokesButton.addEventListener("click",funnyJokes);
+function funnyJokes() {
+    var jokes = "https://api.chucknorris.io/jokes/random"
+    fetch(jokes)
+    .then(response => response.json())
+
+    .then(function(data) {
+        displayFunnyJoke(data)
+    });
+    
+    document.getElementById("button-container").style.display = "none";
+    document.getElementById("back-button").style.display = "block";
+};
+
+function displayFunnyJoke(funnyStuff) {
+    var firstFunnyThing = funnyStuff.value;
+    // we should discuss changing "catFact" to a more universal name
+    document.body.appendChild(catFact);
+    catFact.innerHTML = firstFunnyThing;
+}
+
+
+    .then(data => console.log(data));
+    document.getElementById("button-container").style.display = "none"; 
+};
+
+function goBackToMainPage() {
+    window.location.reload();
+}
+
 dogPicsButton.addEventListener("click", getDogPicture );
 
-funnyJokesButton.addEventListener("click", );
+// funnyJokesButton.addEventListener("click", );
 
 catFactsButton.addEventListener("click", getCatFact);
 
